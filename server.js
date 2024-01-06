8fb1d60b23d9a3b9a5df3c765214ca9eaa088b7f
shivam12166 <94752883+shivam12166@users.noreply.github.com>
2024-01-06 21:25:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from "./routes/productRoutes.js"
 import cors from 'cors';
 import path from 'path';
+const __dirname = path.resolve();
 //configure env
 dotenv.config();
 
